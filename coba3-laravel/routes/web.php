e710f45d36716e77fa72c36be62c8f6fe6652d6d
aimar25 <aimarsak@gmail.com>
2021-12-02 22:09:51 +0700

    telah menyelesaikan video 23

--- coba3-laravel/routes/web.php
+++ coba3-laravel/routes/web.php
@@ -67,3 +67,6 @@ Route::get('/dashboard/posts/checkSlug', [DashboardPostController::class, 'check
 
 Route::resource('/dashboard/posts', DashboardPostController::class)
 ->middleware('auth');
+
+Route::resource('/dashboard/categories', AdminCategoryController::class)->except('show')
+->middleware('admin');
